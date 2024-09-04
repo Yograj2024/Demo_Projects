20ac61bc48c1449c46a49c69ec3e00cc604cae24
Yograj Singh <ryog7910@gmail.com>
2024-09-03 20:33:25 -0800

    script.js

--- 04_project/script.js
+++ 04_project/script.js
@@ -1,24 +1,22 @@
 
-let listbar=document.getElementById("menuList");
-console.log(listbar)
+let sideBar=document.getElementsByClassName("sideBarHide")[0];
 let list=document.getElementById("list");
 
 document.getElementsByClassName("menuIcon")[0].addEventListener("click",()=>{
-    listbar.classList.add("show");
-    console.log(listbar);
+    sideBar.classList.add("show")
     document.body.style.overflow="hidden";
 });
 
-let remove=function (){
-    listbar.classList.remove("show");
+let hide=function(){
+    sideBar.classList.remove("show")
     document.body.style.overflow="auto";
 }
 
-document.getElementById("clsoeIcon").addEventListener("click",remove)
+document.getElementById("clsoeIcon").addEventListener("click",hide)
 
 list.addEventListener("click",(e)=>{
     if(e.target.tagName==="A"){
-        remove();
+        hide();
         return;
     }
-})
+});
